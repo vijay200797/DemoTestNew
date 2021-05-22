6500e0e6943a1010851add0e8e937fd301d2caa4
vijay200797@gmail.com <vijay200797@gmail.com>
2021-05-22 14:18:30 +0530

    first commit

--- DemoTestApp/settings.py
+++ DemoTestApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h0&c1$297ul#%n0pl710v0hh9!=6^mpr(#!ok2c%c15g7wve^z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DemoTestStudent'
 ]
 
 MIDDLEWARE = [
--- DemoTestApp/urls.py
+++ DemoTestApp/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from DemoTestStudent import views
+    
+
+
 
 urlpatterns = [
+    path('', views.vw_demostudent),
     path('admin/', admin.site.urls),
 ]
